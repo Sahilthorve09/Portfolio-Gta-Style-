d471d787b1743a4e5a54286063a48ccd8d6deaa7
Sahil_Thorve_09 <149391162+Sahilthorve09@users.noreply.github.com>
2025-06-07 18:46:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Portfolio-Gta-Style-
-',
+  base: '/Portfolio-Gta-Style-/',
 })
